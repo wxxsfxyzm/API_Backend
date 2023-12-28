49a7595f3cd70287514426c0dcc7890bcdd22d9d
wxxsfxyzm <wxxsfxyzm@126.com>
2023-12-29 01:34:43 +0800

    remoteApiAccessHandler

--- src/main/kotlin/com/carlyu/apibackend/service/APIRemoteAccess.kt
+++ src/main/kotlin/com/carlyu/apibackend/service/APIRemoteAccess.kt
@@ -2,6 +2,7 @@ package com.carlyu.apibackend.service
 
 import com.carlyu.apibackend.dto.GoogleApiRequestDTO
 
+
 interface APIRemoteAccess {
 
     fun visitRemoteAPI(url: String, key: String, requestBody: GoogleApiRequestDTO): String
--- src/main/kotlin/com/carlyu/apibackend/service/impl/APIRemoteResponseHandlerImpl.kt
+++ src/main/kotlin/com/carlyu/apibackend/service/impl/APIRemoteResponseHandlerImpl.kt
@@ -1,12 +1,29 @@
 package com.carlyu.apibackend.service.impl
 
+import com.carlyu.apibackend.dto.GoogleApiRequestDTO
+import com.carlyu.apibackend.enums.url
+import com.carlyu.apibackend.service.APIRemoteAccess
 import com.carlyu.apibackend.service.APIRemoteResponseHandler
-import com.carlyu.apibackend.vo.ApiResponseVO
+import com.carlyu.apibackend.utils.toUser
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
 
 @Service
-class APIRemoteResponseHandlerImpl : APIRemoteResponseHandler {
-    override fun handleResponse(responseVO: ApiResponseVO): String {
-        return responseVO.apiResponse
-    }
+class APIRemoteResponseHandlerImpl(
+    private val apiRemoteAccess: APIRemoteAccess
+) : APIRemoteResponseHandler {
+    override fun handleResponse(
+        authentication: Authentication,
+        text: String
+    ): String = jacksonObjectMapper()
+        .readTree(
+            apiRemoteAccess.visitRemoteAPI(
+                url.GOOGLE_GEMINI_API.url,
+                authentication.toUser().googleApiKey,
+                GoogleApiRequestDTO.createBody(text)
+            )
+        )
+        .findValuesAsText("text")[0].toString()
+
 }
