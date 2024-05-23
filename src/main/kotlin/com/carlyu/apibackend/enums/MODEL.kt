347447e70be42a0ef543b27db9717ca317b4ea56
wxxsfxyzm <wxxsfxyzm@126.com>
2024-05-23 12:21:01 +0800

    add model enums

--- src/main/kotlin/com/carlyu/apibackend/enums/MODEL.kt
+++ src/main/kotlin/com/carlyu/apibackend/enums/MODEL.kt
@@ -4,5 +4,6 @@ import com.carlyu.apibackend.constant.BaseConstant
 
 enum class MODEL(val model: String) {
     GEMINI_PRO(BaseConstant.GOOGLE_GEMINI_MODEL_GEMINI_PRO),
-    GEMINI_PRO_VISION(BaseConstant.GOOGLE_GEMINI_MODEL_GEMINI_PRO_VISION)
+    GEMINI_PRO_VISION(BaseConstant.GOOGLE_GEMINI_MODEL_GEMINI_PRO_VISION),
+    GEMINI_1_5_PRO(BaseConstant.GOOGLE_GEMINI_MODEL_GEMINI_1_5_PRO)
 }
